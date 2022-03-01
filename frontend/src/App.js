de980b81144482446e21f352efebe19bba4b8969
Maheen Hoque <75400940+NotAlpha45@users.noreply.github.com>
2022-03-02 00:48:05 +0600

    Some debugging in the frontend
    Fixed issue with topic and bubble sort routing.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,9 +8,11 @@ import SignIn from "./components/Auth/SignIn";
 import Topics from "./components/Topics/Topics";
 import Playground from "./components/Playground/Playground";
 // import Editor from "./components/Editor/Editor";
-import Blog from "./components/Blog/Blog"
-const App = () => (
+import Blog from "./components/Blog/Blog";
+import Algorithm from "./components/Algorithm/Algorithm";
+import BubbleSort from "./components/Algorithm/BubbleSort/BubbleSort";
 
+const App = () => (
   <BrowserRouter>
     <Container maxWidth="lg">
       <Navbar />
@@ -22,7 +24,8 @@ const App = () => (
         <Route path="/topics" element={<Topics />} />
         <Route path="/playground" element={<Playground />} />
         <Route path="/blog" element={<Blog />} />
-
+        <Route path="/algorithm" element={<Algorithm />} />
+        <Route path="/bubble_sort" element={<BubbleSort />} />
         {/* <Route path="/editor" element={<Editor />} /> */}
       </Routes>
     </Container>
