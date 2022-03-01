7da08324af93be90c159762e28630483fddbffe4
Maheen Hoque <75400940+NotAlpha45@users.noreply.github.com>
2022-03-02 00:21:36 +0600

    Scrapped and redesigned the backend

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,22 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
-
-import postRoutes from "./routes/posts.js";
-import getHome from "./routes/home.js";
-import userRoutes from "./routes/users.js";
-
+require("dotenv").config();
+const express = require("express");
 const app = express();
+const cors = require("cors");
+const connection = require("./db");
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+// database connection
+connection();
+
+// middlewares
+app.use(express.json());
 app.use(cors());
 
-app.use("/", getHome);
-app.use("/posts", postRoutes);
 
-// Routes to user related paths (Signup, sign in etc)
-app.use("/user", userRoutes);
+// routes
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 
-// Database connection
-const CONNECTION_URL =
-  "mongodb+srv://maheen:maheen123@cluster0.uoc1c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 5000;
-
-mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() =>
-    app.listen(PORT, () =>
-      console.log(`Server Running on Port: http://localhost:${PORT}`)
-    )
-  )
-  .catch((error) => console.log(`${error} did not connect`));
-
-// mongoose.set('useFindAndModify', false);
+const port = process.env.PORT || 8080;
+app.listen(port, console.log(`Listening on port ${port}...`));
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,10 +1,27 @@
-import express from 'express';
-import {signin, signup} from "../controllers/user.js"
+const router = require("express").Router();
+const { User, validate } = require("../models/user");
+const bcrypt = require("bcrypt");
 
-const router = express.Router()
+router.post("/", async (req, res) => {
+	try {
+		const { error } = validate(req.body);
+		if (error)
+			return res.status(400).send({ message: error.details[0].message });
 
-// Sign in is sending data, hence post
-router.post('/signin', signin)
-router.post('/signup', signup);
+		const user = await User.findOne({ email: req.body.email });
+		if (user)
+			return res
+				.status(409)
+				.send({ message: "User with given email already Exist!" });
 
-export default router;
+		const salt = await bcrypt.genSalt(Number(process.env.SALT));
+		const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+		await new User({ ...req.body, password: hashPassword }).save();
+		res.status(201).send({ message: "User created successfully" });
+	} catch (error) {
+		res.status(500).send({ message: "Internal Server Error" });
+	}
+});
+
+module.exports = router;
